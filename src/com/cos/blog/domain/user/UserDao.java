bd2322963f3af16eb5136140513ed77c8c25729e
LeeBG <donny1848@gmail.com>
2021-01-04 15:05:08 +0900

    오류수정중

--- src/com/cos/blog/domain/user/UserDao.java
+++ src/com/cos/blog/domain/user/UserDao.java
@@ -2,11 +2,33 @@ package com.cos.blog.domain.user;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.cos.blog.config.DB;
 import com.cos.blog.domain.user.dto.JoinReqDto;
 
 public class UserDao {
+	public int findByUsername(String username) {
+		String sql = "SELECT * from user WHERE username = ?";
+		Connection conn = DB.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, username);
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) { //참이면
+				return 1;	//있어
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			DB.close(conn,pstmt,rs);
+		}
+		return -1;
+	}
 	public int save(JoinReqDto dto) { //회원가입
 		String sql = "INSERT INTO user(username,password,email,address,userRole,createDate) VALUES(?,?,?,?,'USER',now())";
 		Connection conn = DB.getConnection();
